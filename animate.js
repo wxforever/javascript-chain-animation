92cf9a9fb5b61f8977c89ff479557cbf81d707a0
ng <1062877403@qq.com>
2016-06-23 17:59:26 +0800

    add some function for the chain animation

--- animate.js
+++ animate.js
@@ -7,9 +7,23 @@
 	
 	function animate(animation){
 
-		
-		var _this=this;
-		
+			var _this=this;
+			
+		_this.reset?'':_this.reset=function(){
+			this.chain=0;
+			return this;
+		};
+		_this.origin?'':_this.origin=function(){//恢复原来状态
+			this.style.cssText='';
+			return this;
+		};
+		_this.timer?'':_this.timer=[];//计时器数组
+		_this.clearanim?'':_this.clearanim=function(){//清楚所有计时器
+                  while(this.timer.length>0){
+                  	 clearTimeout(this.timer.pop());
+                  }
+                  return this;
+		}
         var animation=animation;
         var paramArr=animation.split(' ');
 		var paramkey=paramArr.slice(0,1);
@@ -26,7 +40,7 @@
 			
 			var chain=_this.chain;//复制chain
 			_this.chain+=(paramArr[3]?parseInt(paramArr[1])+parseInt(paramArr[1]):parseInt(paramArr[1]))*1000;
-			setTimeout(function(){
+			var timer=setTimeout(function(){
          var paramArr=animation.split(' ');
 		var paramkey=paramArr.slice(0,1);
 		var paramValue=paramArr.splice(1,1);
@@ -35,6 +49,8 @@
         _this.style.cssText+='-webkit-transition:'+cssText+';'+paramkey+':'+paramValue+';';
        
 			},chain);
+
+			_this.timer.push(timer);
 			return _this;
 		}
 		
